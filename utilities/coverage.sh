e64cadd99fa7d194efc48c5e7f946eabcba05aed
Marco Ratto <marcoratto@gmail.com>
2025-09-18 09:41:18 +0200

    Some file updated

--- utilities/coverage.sh
+++ utilities/coverage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source env/bin/activate
+export PYTHONPATH="${PYTHONPATH}:$(pwd)/src"
 coverage run -m unittest ./tests/unit_test_publisher.py 
 coverage run --append -m unittest ./tests/unit_test_subscriber.py
 coverage report 
